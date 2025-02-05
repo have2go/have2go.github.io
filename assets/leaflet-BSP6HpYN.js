09fd3bedd4138da9e1c4c8c356bd45ec45860f41
have2go <boxadmail@yandex.ru>
2025-02-05 21:49:50 +0300

    fix markers for prod

---
+++ assets/leaflet-BSP6HpYN.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-3fevySbJ.js";import{L as r}from"./leaflet.draw-BndfyFCs.js";const f=t(({app:o})=>{o.config.globalProperties.$L=r});export{f as default};
---
+++ assets/pinia-CV7NAk-h.js
@@ -0,0 +1,1 @@
+import{b as o}from"./index-3fevySbJ.js";import{c as t}from"./index-BPn8v8xY.js";const s=o(({app:a})=>{const i=t();a.use(i)}),m={name:"Pinia"};export{s as default,m as meta};
